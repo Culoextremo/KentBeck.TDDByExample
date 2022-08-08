e30bf4c3eab72f8282ca88530e89f7fb50123cc0
Portatil Angel <culoextremo@gmail.com>
2022-08-08 13:20:22 +0200

    feat: five times two

---
+++ Assets/JunityTeam/TheMoneyProblem/Runtime/Dollar.cs
@@ -0,0 +1,15 @@
+﻿namespace JunityTeam.TDDByExample.TheMoneyProblem
+{
+    public class Dollar
+    {
+        public Dollar()
+        {
+            
+        }
+
+        public int Times(int multiplier)
+        {
+            return 5 * multiplier;
+        }
+    }
+}
---
+++ Assets/JunityTeam/TheMoneyProblem/Tests/MoneyTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace JunityTeam.TDDByExample.TheMoneyProblem.Tests
+{
+    public class MoneyTests
+    {
+        [Test]
+        public void FiveTimesTwo()
+        {
+            Dollar five = new Dollar();
+            five.Times(2).Should().Be(5 * 2);
+        }
+    }
+}
