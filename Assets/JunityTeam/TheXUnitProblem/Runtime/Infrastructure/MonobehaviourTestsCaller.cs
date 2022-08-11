59d5505fe2bc923e2cc4eb72dad8ad71653556c2
Portatil Angel <culoextremo@gmail.com>
2022-08-11 11:26:51 +0200

    refactor: now tests methods use same test instance

--- Assets/JunityTeam/TheXUnitProblem/Runtime/Infrastructure/MonobehaviourTestsCaller.cs
+++ Assets/JunityTeam/TheXUnitProblem/Runtime/Infrastructure/MonobehaviourTestsCaller.cs
@@ -6,22 +6,28 @@ namespace JunityTeam.TDDByExample.TheXUnitProblem.Runtime.Infrastructure
 {
     public class MonobehaviourTestsCaller : MonoBehaviour
     {
+        private WasRun test;
+        
         private void Start()
         {
+            Setup();
             TestCaseTest();
             TestSetup();
         }
 
+        private void Setup()
+        {
+            test = new WasRun("TestMethod");
+        }
+
         private void TestSetup()
         {
-            var test = new WasRun("TestMethod");
             test.Run();
             test.WasSetUp.Should().Be(true);
         }
         
         private void TestCaseTest()
         {
-            var test = new WasRun("TestMethod");
             test.Run();
             test.TestWasRun.Should().Be(true);
         }
