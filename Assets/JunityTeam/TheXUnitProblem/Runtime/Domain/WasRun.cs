c5d31ef6739c45a77aced0191e02655f60220dc7
Portatil Angel <culoextremo@gmail.com>
2022-08-11 11:55:06 +0200

    refactor: removed test was run property

--- Assets/JunityTeam/TheXUnitProblem/Runtime/Domain/WasRun.cs
+++ Assets/JunityTeam/TheXUnitProblem/Runtime/Domain/WasRun.cs
@@ -2,23 +2,19 @@
 {
     public class WasRun : TestCase
     {
-        public bool TestWasRun { get; private set; }
         public string Log { get; private set; }
 
         public WasRun(string name) : base(name)
         {
-            TestWasRun = false;
         }
 
         public override void SetUp()
         {
-            TestWasRun = false;
             Log = "SetUp ";
         }
         
         public void TestMethod()
         {
-            TestWasRun = true;
             Log += "TestMethod ";
         }
 
