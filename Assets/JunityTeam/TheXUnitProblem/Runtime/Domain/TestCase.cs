f0ad48b7e6788dc392bcbdfb70716576260da383
Portatil Angel <culoextremo@gmail.com>
2022-08-11 10:53:53 +0200

    refactor: test case superclass

---
+++ Assets/JunityTeam/TheXUnitProblem/Runtime/Domain/TestCase.cs
@@ -0,0 +1,17 @@
+﻿namespace JunityTeam.TDDByExample.TheXUnitProblem.Runtime.Domain
+{
+    public class TestCase
+    {
+        public readonly string Name;
+        protected TestCase(string name)
+        {
+            Name = name;
+        }
+        
+        public void Run()
+        {
+            var method = GetType().GetMethod(Name);
+            method.Invoke(this, null);
+        }
+    }
+}
--- Assets/JunityTeam/TheXUnitProblem/Runtime/Domain/WasRun.cs
+++ Assets/JunityTeam/TheXUnitProblem/Runtime/Domain/WasRun.cs
@@ -1,21 +1,13 @@
 ﻿namespace JunityTeam.TDDByExample.TheXUnitProblem.Runtime.Domain
 {
-    public class WasRun
+    public class WasRun : TestCase
     {
         public bool TestWasRun { get; private set; }
-        public readonly string Name;
-
-        public WasRun(string name)
+        public WasRun(string name) : base(name)
         {
             TestWasRun = false;
-            Name = name;
         }
-
-        public void Run()
-        {
-            var method = GetType().GetMethod(Name);
-            method.Invoke(this, null);
-        }
+        
         public void TestMethod()
         {
             TestWasRun = true;
